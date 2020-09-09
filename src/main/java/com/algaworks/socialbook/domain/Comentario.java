c5cde78b8358cafc10d51e5178700563417741c3
Constantinoi <pescaalma@gmail.com>
2020-09-09 12:12:48 -0400

    Criando uma representação para o recurso Livro

---
+++ src/main/java/com/algaworks/socialbook/domain/Comentario.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.algaworks.socialbook.domain;
+
+import java.util.Date;
+
+/**
+ *
+ * @author stirr
+ */
+class Comentario {
+
+    private Long id;
+    private String texto;
+    private String usuario;
+    private Date data;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+}
--- src/main/java/com/algaworks/socialbook/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbook/resources/LivrosResources.java
@@ -5,6 +5,9 @@
  */
 package com.algaworks.socialbook.resources;
 
+import com.algaworks.socialbook.domain.Livro;
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class LivrosResources {
 
     @RequestMapping(value = "/livros", method = RequestMethod.GET)
-    public String listar() {
-        return "Rest aplicado, Git passo a passo";
+    public List<Livro> listar() {
+        
+        Livro l1 = new Livro("Rest aplicado");
+        Livro l2 = new Livro("Git passo a passo");
+        
+        Livro[] livros = {l1, l2};
+        
+        return Arrays.asList(livros);
     }
 }
